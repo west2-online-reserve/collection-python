9bdb0f7b26616392377309adda99d47c1cbf25a6
RunForTime2023 <144543042+RunForTime2023@users.noreply.github.com>
2023-10-04 20:07:35 +0800

    Add files via upload

--- work1/RunForTime2023/decorator.py
+++ work1/RunForTime2023/decorator.py
@@ -1,14 +1,21 @@
 import time
-def Test():
+
+
+def Test(func):
     def Decorator():
-        a=time.localtime()
-        print("Function Name:",Decorator.__name__,"\n")
-        time.sleep(3)
-        b=time.localtime()
-        print("Start Time:",time.strftime("%Y/%m/%d %H:%M:%S",a))
-        print("End Time:", time.strftime("%Y/%m/%d %H:%M:%S",b))
-        print("Time Interval:",time.perf_counter(),"s")
+        a = time.localtime()
+        result = func();
+        b = time.localtime()
+        print("Start Time:\t\t", time.strftime("%Y/%m/%d %H:%M:%S", a))
+        print("End Time:\t\t", time.strftime("%Y/%m/%d %H:%M:%S", b))
+        print("Time Interval:\t", time.perf_counter(), "s")
+        return result
     return Decorator
 
-t=Test();
-t();
+
+@Test
+def execute():
+    print("Function Name:\t", execute.__name__, "\n")
+    time.sleep(3)
+
+execute()
