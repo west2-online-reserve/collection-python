72b8e4fe2b1ff7a1cd2bb54c503d11801483e90d
ding41 <144247527+ding41@users.noreply.github.com>
2023-10-05 11:22:22 +0800

    Update 06.py

--- work1/ding41/06.py
+++ work1/ding41/06.py
@@ -1,7 +1,6 @@
 def f(lists) :
-    a=len(lists)
-    b={"数字个数": a}
-    print(b)
-
-m=[7,8,9,5,11]
-f(m)
+    dict={}
+    for i in lists :
+        a=lists.count(i)
+        dict[f"{i}的个数"]=a
+    print(dict)
