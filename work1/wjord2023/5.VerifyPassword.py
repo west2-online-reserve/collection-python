89731e670b73f952fe6b757defc60003947115f5
wjord <141616391+wjord2023@users.noreply.github.com>
2023-10-05 19:36:10 +0800

    :clown_face:

--- work1/wjord2023/5.VerifyPassword.py
+++ work1/wjord2023/5.VerifyPassword.py
@@ -1,19 +1,9 @@
 import re
 content = input("请输入密码：")
-if len(content) > 18 or len(content) < 6:
-    print("你输入的密码不符合规范！")
+r = re.match(r"^[a-zA-Z0-9]{6,18}$",content)
+if r:
+    print("密码已通过验证")
 else:
-    a = re.findall("_",content)
-    # 检验密码中是否有_
-    if len(a) == 0:
-        s1 = re.sub('\w+', '', content)
-        # 将字母数字去除观察是否还有其他类型的数据（w+对应数字，字母和_）
-        if len(s1) == 0:
-            print("你的密码已通过认证！")
-        else:
-            print("你输入的密码不符合规范！")
-    else:
-        print("你输入的密码不符合规范！")
-
+    print("密码不符合规范")
 
 
