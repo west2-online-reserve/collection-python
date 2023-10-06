76fc10d9f42789c8d436c34f8c72cfc0d2a684f9
Lotussara <143892871+Lotussara@users.noreply.github.com>
2023-10-06 21:06:50 +0800

    Update 05.py

--- work1/Lotussara/05.py
+++ work1/Lotussara/05.py
@@ -1,10 +1,6 @@
-dictA={}
-dictA['102301201']="Ben"
-dictA["102301202"]="John"
-dictA['102301203']='Jenny'
-dictA['102301204']='Ocean'
-dictA['102301206']='Billy'
-del dictA['102301202']
-del dictA['102301204']
-del dictA['102301206']
-print(dictA)
+dict={101:"甲",102:"乙",103:"丙",104:"丁"}
+for num in list(dict):
+    if num%2==0:
+        del dict[num]
+print(dict)
+
