dcff8dc0cadafbc1073e5bb19507c09b966810de
Starryship <108072403+Starryship@users.noreply.github.com>
2023-10-06 16:24:47 +0800

    Update 5-RegexPassword.py

--- work1/Starryship/5-RegexPassword.py
+++ work1/Starryship/5-RegexPassword.py
@@ -8,10 +8,7 @@ import re
 password=input("请输入你的密码：")
 
 #计算密码长度，传参数进入正则表达式，保证使用match从头开始检查密码的时候可以检查该密码是否全部为英文和数字
-Length=len(password)
-lst3='[a-zA-Z0-9]{%d}'%(Length if Length>=6 and Length<=18 else -1)
-
-if not(re.match(lst3,password)):
+if not(re.match(r"^[a-zA-Z0-9]{6,18}$",password)):
     print("校验失败，密码格式不符合要求，请重新输入")
 else:
     print("校验成功，密码格式符合要求")
