afdb4c4211964b5be4e3ffe3be4952d9763f63ba
kexinglongwo <123803157+kexinglongwo@users.noreply.github.com>
2023-10-07 19:11:36 +0800

    Update 4-魔术方法.py

--- work1/kexinglongwo/4-魔术方法.py
+++ work1/kexinglongwo/4-魔术方法.py
@@ -7,7 +7,21 @@ class MyZoo:
             return True
         else:
             return False
+
+    def __str__(self) :
+        t=''
+        for i,o in self.anmials.items():
+            t=t+(f"动物种类有:{i}  动物数量为：{o}")+'\n'
+        return t
+
+    def __len__(self):
+        t =0
+        for i, o in self.anmials.items():
+            t+=o
+        return t
 a=MyZoo({'dog':2,"pig":1})
 b=MyZoo({"pig":1,'dog':2})
 print(a==b)
+print(a.__str__())
+print(a.__len__())
 #github:2877378857qq.com   西二--作业
