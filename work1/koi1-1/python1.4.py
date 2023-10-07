f36a347fddaacb4c627009e08cfa0b2214a0bd51
Laishiting <1413365793@qq.com>
2023-10-08 00:08:43 +0800

    add: complete work1

--- work1/koi1-1/python1.4.py
+++ work1/koi1-1/python1.4.py
@@ -1,5 +1,11 @@
-my_list = ["python", 66, "hello", 10, 16]
-del my_list[0]
-my_list.pop(1)
-sorted_list = sorted(my_list)
+list1 = input("请输入一个包含字符串和整数的列表，用逗号分隔：").split(",")
+list2 = []
+for i in list1:
+    try:
+        num = int(i)
+        list2.append(i)
+    except ValueError:
+        pass
+sorted_list = sorted(list2)
 print(sorted_list)
+
