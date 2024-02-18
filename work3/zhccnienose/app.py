8583ae221557692dfc945237cc9a92dba7ecb636
ingggg <729479632@qq.com>
2024-02-18 22:22:58 +0800

    fix:根据页码查询

--- work3/zhccnienose/app.py
+++ work3/zhccnienose/app.py
@@ -49,7 +49,7 @@ def delete_task_status():
 
     db.session.commit()
 
-    return jsonify(code=200, msg="删除成功"),200
+    return jsonify(code=200, msg="删除成功"), 200
 
 
 # 删除所有事项
@@ -80,59 +80,59 @@ def get_task(id):
 # 根据关键字模糊查询
 @app.route('/task', methods=['GET'])
 def get_task_keyword():
-    keyword = request.args.get('keyword')
+    keyword = request.args.get('keyword', type=str)
     # 每页数量
     per_page = request.args.get('per_page', type=int)
+    # 第几页
+    page = request.args.get('page', type=int)
+    if page <= 0:
+        return jsonify(code=400, msg="参数错误"), 400
+    # 文章总数
+    cot = Task.query.count()
+    # 校验
+    if cot % per_page == 0 and page > cot // per_page:
+        return jsonify(code=400, msg="页数超出范围"), 400
+    elif cot % per_page != 0 and page > (cot // per_page) + 1:
+        return jsonify(code=400, msg="页数超出范围"), 400
 
     # 标题或内容包含关键字
-    tasks = Task.query.filter(or_(Task.title.contains(keyword), Task.content.contains(keyword)))
+    tasks = (Task.query.filter(or_(Task.title.contains(keyword),
+                                   Task.content.contains(keyword)))
+             .paginate(page=page, per_page=per_page, max_per_page=cot, error_out=False))
 
-    cot = tasks.count()
-    if cot % per_page != 0:
-        pages = cot // per_page + 1
-    else:
-        pages = cot // per_page
-
-    tasks = tasks.paginate(per_page=per_page, error_out=False)
     list_task = []
 
-    for i in range(pages):
-        data = tasks.items
-        list_task.append([])
-        for task in data:
-            list_task[i].append(task.data())
-
-        tasks = tasks.next()
+    data = tasks.items
+    for task in data:
+        list_task.append(task.data())
 
     return jsonify(code=200, msg="success", data=list_task)
 
 
-# 根据完成状态查询事项
 @app.route('/task/status', methods=['GET'])
 def get_task_status():
-    # 完成状态
-    status = request.args.get('status')
+    status = request.args.get('status', type=str)
     if status != "undone" and status != "done":
         return jsonify(code=400, msg="参数错误"), 400
-    # 每页数量
     per_page = request.args.get('per_page', type=int)
+    page = request.args.get('page', type=int)
 
-    cot = Task.query.filter_by(status=status).count()
-    if cot % per_page != 0:
-        pages = cot // per_page + 1
+    # 文章总数
+    cot = Task.query.count()
+    # 校验
+    if page <= 0:
+        return jsonify(code=400, msg="参数错误"), 400
+    elif cot % per_page == 0 and page > cot // per_page:
+        return jsonify(code=400, msg="页数超出范围"), 400
+    elif cot % per_page != 0 and page > (cot // per_page) + 1:
+        return jsonify(code=400, msg="页数超出范围"), 400
     else:
-        pages = cot // per_page
+        pass
+    tasks = Task.query.filter_by(status=status).paginate(page=page, per_page=per_page, error_out=False).items
 
-    tasks = Task.query.filter_by(status=status).paginate(per_page=per_page, error_out=False)
     list_task = []
-
-    for i in range(pages):
-        data = tasks.items
-        list_task.append([])
-        for task in data:
-            list_task[i].append(task.data())
-
-        tasks = tasks.next()
+    for task in tasks:
+        list_task.append(task.data())
 
     return jsonify(code=200, msg="success", data=list_task)
 
@@ -185,7 +185,7 @@ def update_task_status():
     try:
         old_status = request.args.get('old_status', type=str)
         if old_status != "undone" and old_status != "done":
-            return jsonify(code=400, msg="参数错误"),400
+            return jsonify(code=400, msg="参数错误"), 400
 
         if old_status == "undone":
             new_status = "done"
